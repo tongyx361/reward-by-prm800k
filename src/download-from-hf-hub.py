fe6cfea98168035dbc575fc3efb114ea651d9cb2
Shawn Yuxuan Tong <tongyuxuan361@gmail.com>
2023-08-16 10:06:54 +0000

    to merge

---
+++ src/download-from-hf-hub.py
@@ -0,0 +1,15 @@
+from huggingface_hub import login
+from transformers import AutoModel, AutoTokenizer
+
+login(token="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")
+
+tokenizer_name = "hf-internal-testing/llama-tokenizer"
+
+tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, resume_download=True)
+
+# model_name = "TheBloke/Llama-2-70B-fp16"
+model_name = "meta-llama/Llama-2-7b-hf"
+
+model = AutoModel.from_pretrained(
+    model_name, resume_download=True, low_cpu_mem_usage=True
+)
--- src/download-model.py
+++
@@ -1,7 +0,0 @@
-from transformers import AutoModel
-from huggingface_hub import login
-
-login(token="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")
-
-model_name = "TheBloke/Llama-2-70B-fp16"
-model = AutoModel.from_pretrained(model_name, resume_download=True)
