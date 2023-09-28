ac384cd6cefbecc67c672cd1010de370cb2b9e3e
Shawn Yuxuan Tong <tongyuxuan361@gmail.com>
2023-09-28 14:36:06 +0000

    to merge

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,10 +1,16 @@
 # every run
 unset TRANSFORMERS_OFFLINE
 # export TRANSFORMERS_OFFLINE=1
+
+# wandb
+
 unset WANDB_TAGS
 WANBD_TAGS="important"
 unset WANDB_MODE
 # export WANDB_MODE='offline'
+
+# devices
+
 # DEVICE_IDS="0,4,5,6"
 DEVICE_IDS="0,1"
 unset CUDA_VISIBLE_DEVICES
@@ -12,8 +18,9 @@ unset CUDA_VISIBLE_DEVICES
 # NUM_GPUS=4
 # NUM_GPUS=2
 NUM_GPUS=$(echo "$DEVICE_IDS" | tr ',' '\n' | grep -c '^[0-9]*$')
-DATE="2023-09-24"
-IDX=10
+# DATE="2023-09-24"
+# IDX=10
+TIME_STAMP=$(date +"%Y-%m-%d-%H-%M-%S")
 # export TORCH_DISTRIBUTED_DEBUG="DETAIL"
 
 # DEBUG='True'
@@ -51,12 +58,10 @@ MODEL_NAME="mammoth"
 # MODEL_PATH="/data/users/zhangjunlei/tyx/.cache/huggingface/hub/models--WizardLM--WizardMath-70B-V1.0/snapshots/e089c3f9d2ad9d1acb62425aec3f4126f498f4c5"
 # MODEL_PATH="/data/users/zhangjunlei/tyx/.cache/huggingface/hub/models--TIGER-Lab--MAmmoTH-70B/snapshots/350ab0bc95339af5e17d38f3176811b77b7d7a16"
 
-
 # MODEL_SIZE_B="13"
 # MODEL_PATH="/data/users/zhangjunlei/tyx/.cache/huggingface/hub/models--meta-llama--Llama-2-13b-hf/snapshots/db6b8eb1feabb38985fdf785a89895959e944936"
 # MODEL_PATH="/data/users/zhangjunlei/tyx/.cache/huggingface/hub/models--WizardLM--WizardMath-13B-V1.0/snapshots/7ef412d2c680ef0fbdcd88d0df31b396d8d3049c"
 # MODEL_PATH="WizardLM/WizardMath-13B-V1.0"
-
 
 MODEL_SIZE_B="7"
 # MODEL_PATH="/data/users/zhangjunlei/tyx/.cache/huggingface/hub/models--meta-llama--Llama-2-7b-hf/snapshots/6fdf2e60f86ff2481f2241aaee459f85b5b0bbb9"
@@ -74,17 +79,14 @@ else
     QLORA="False"
 fi
 
-
 # ACCELERATE_LIB="flash-attn-v2"
 ACCELERATE_LIB="xformers"
 # ACCELERATE_LIB="flash-attn-v1"
 # ACCELERATE_LIB=""
 
-
-
 # ENCODED_DATASETS_NAME="encoded-datasets-direct-prediction"
-DATASET_NAME="prm800k-train-direct-prediction-0-02validiation"
-ENCODED_DATASETS_NAME="${DATASET_NAME}-encoded-datasets"
+DATASETS_NAME="prm800k-train-direct-prediction-0-02validiation"
+ENCODED_DATASETS_NAME="${DATASETS_NAME}-encoded-datasets"
 # TRAIN_BATCH_SIZE_PER_GPU=16
 TRAIN_BATCH_SIZE_PER_GPU=2 # 7B ZeRO-3 >= 2
 # EVAL_BATCH_SIZE_PER_GPU=4
@@ -92,7 +94,7 @@ EVAL_BATCH_SIZE_PER_GPU=8 # 7B >= 128, but 8/16/128 have almost the same perform
 TOTAL_BATCH_SIZE=128
 # TOTAL_BATCH_SIZE=120
 # TOTAL_BATCH_SIZE=96
-GRADIENT_ACC_STEPS=$(($TOTAL_BATCH_SIZE/$NUM_GPUS/$TRAIN_BATCH_SIZE_PER_GPU))
+GRADIENT_ACC_STEPS=$(($TOTAL_BATCH_SIZE / $NUM_GPUS / $TRAIN_BATCH_SIZE_PER_GPU))
 NUM_TRAIN_EPOCHS=100
 
 # LR="2e-5"
@@ -109,20 +111,22 @@ LORA_DROPOUT=0.05
 
 # 以能继续训练为同一个实验
 
+# experiment name
+
 LR_INFO="lr=${LR}-schdule=${LR_SCHEDULE}-wd=${WEIGHT_DECAY}-warmup=${WARMUP}"
 LORA_INFO="lora-modules=${LORA_MODULES}-r=${LORA_R}-alpha=${LORA_ALPHA}-dropout=${LORA_DROPOUT}"
-HYPER_PARAMS_INFO="bs=${TOTAL_BATCH_SIZE}-gas=${GRADIENT_ACC_STEPS}"
+HYPER_PARAMS_INFO="bs=${TOTAL_BATCH_SIZE}-gas=${GRADIENT_ACC_STEPS}-${LR_INFO}"
 if [ -z "${PEFT}" ]; then
-    EXP_NAME="${MODEL_NAME}-${MODEL_SIZE_B}b-${DATASET_NAME}-${HYPER_PARAMS_INFO}"
+    EXP_NAME="${MODEL_NAME}-${MODEL_SIZE_B}b-${DATASETS_NAME}-${HYPER_PARAMS_INFO}"
 else
-    HYPER_PARAMS_INFO="${HYPER_PARAMS_INFO}-${LR_INFO}"
     if [[ "${PEFT}" =~ "lora" ]]; then
-        HYPER_PARAMS_INFO="${HYPER_PARAMS_INFO}-${LR_INFO}-${LORA_INFO}"
+        HYPER_PARAMS_INFO="${HYPER_PARAMS_INFO}-${LORA_INFO}"
     fi
-    EXP_NAME="${MODEL_NAME}-${MODEL_SIZE_B}b-${PEFT}-${DATASET_NAME}-${HYPER_PARAMS_INFO}"
+    EXP_NAME="${MODEL_NAME}-${MODEL_SIZE_B}b-${PEFT}-${DATASETS_NAME}-${HYPER_PARAMS_INFO}"
 fi
 
-export WANDB_NAME="${DATE}-${IDX}-${EXP_NAME}"
+# export WANDB_NAME="${DATE}-${IDX}-${EXP_NAME}"
+export WANDB_NAME="${TIME_STAMP}-${EXP_NAME}"
 
 # MAX_SEQ_LENGTH=4096
 MAX_SEQ_LENGTH=1024
@@ -149,8 +153,9 @@ ENCODED_DATASETS_PATH="${PROJECT_DIR}/datasets/${ENCODED_DATASETS_NAME}"
 OUTPUT_DIR="${PROJECT_DIR}/models/${EXP_NAME}"
 LOG_DIR="${PROJECT_DIR}/logs/train/${EXP_NAME}"
 mkdir -p "${LOG_DIR}"
-LOG_NAME="${DATE}-${IDX}.out"
-LOG_PATH="${LOG_DIR}/${LOG_NAME}"
+# LOG_NAME="${DATE}-${IDX}.out"
+# LOG_PATH="${LOG_DIR}/${LOG_NAME}"
+LOG_PATH="${LOG_DIR}/${TIME_STAMP}.out}"
 
 if [[ ${ACCELERATE_LIB} = "flash-attn-v2" ]]; then
     TRAIN_SCRIPT_PATH="${PROJECT_DIR}/src/train_flash_attn_2.py"
@@ -159,7 +164,6 @@ elif [[ ${ACCELERATE_LIB} = "xformers" ]]; then
 else
     TRAIN_SCRIPT_PATH="${PROJECT_DIR}/src/train.py"
 fi
-
 
 # command
 
@@ -173,7 +177,7 @@ nohup \
     deepspeed \
     --include "localhost:${DEVICE_IDS}" \
     "${TRAIN_SCRIPT_PATH}" \
-    --model_name_or_path "$MODEL_PATH"  \
+    --model_name_or_path "$MODEL_PATH" \
     --encoded_datasets_path "$ENCODED_DATASETS_PATH" \
     --output_dir "$OUTPUT_DIR" \
     --model_max_length "$MAX_SEQ_LENGTH" \
@@ -184,7 +188,7 @@ nohup \
     --num_train_epochs "$NUM_TRAIN_EPOCHS" \
     --eval_first ${EVAL_FIRST} \
     --evaluation_strategy "steps" \
-    --eval_steps "$EVAL_STEPS"  \
+    --eval_steps "$EVAL_STEPS" \
     --save_strategy "steps" \
     --save_steps "$EVAL_STEPS" \
     --save_total_limit 1 \
@@ -207,11 +211,11 @@ nohup \
     --logging_strategy "steps" \
     --logging_steps 1 \
     --custom_debug "${DEBUG}" \
-    &> "$LOG_PATH" \
+    &>"$LOG_PATH" \
     &
-    # --fp16 True \
-    # --save_total_limit 2 \
-    # --flash_attn False
-    # --lora_r 256 \
-    # --lora_alpha 256 \
-    # --eval_first False \
+# --fp16 True \
+# --save_total_limit 2 \
+# --flash_attn False
+# --lora_r 256 \
+# --lora_alpha 256 \
+# --eval_first False \
